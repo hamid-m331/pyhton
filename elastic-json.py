83ff00ce868a07bfcf1b76eee897635f38437629
hamid-m331 <h.madahian@charisma.tech>
2022-09-05 10:34:30 +0430

    First

--- elastic-json.py
+++ elastic-json.py
@@ -2,16 +2,18 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 from random import getrandbits
 import json
+import sys
 import time
 import requests
 # Written by Hamid Madahian with help of DevOps team
 es = Elasticsearch(
     'https://elk.charisma.tech:443',
     # ca_certs="/path/to/http_ca.crt"
-    basic_auth=("user", "pass"),
+    basic_auth=("madahian", "123@abc"),
     verify_certs=False
 )
 
+MyFile= open("/home/hamid/Downloads/Sample-employee-JSON-data.json",'r').read()
 URL="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
 get=requests.get(URL)
 
